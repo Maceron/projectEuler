b148156a9a6a573c95b73394ffd79e24bf83909a
Cameron Brown <cameronthebrown@gmail.com>
2019-04-18 11:47:04 +0930

    35 actually solved, last commit was meant for 33

--- 35/35.php
+++ 35/35.php
@@ -7,4 +7,31 @@
   How many circular primes are there below one million?
 */
 
+function isPrime($n){
+  if($n % 2 == 0){
+    return false;
+  }
+  for($i = 3; $i <= ceil($n/$i); $i += 2){
+    if($n % $i == 0){
+      return false;
+    }
+  }
+  return true;
+}
 
+// put 2 in the results already since we start search from 3
+$c = array(2);
+for($i = 3; $i < 1000000; $i += 2){
+  if(!isPrime($i)){
+    continue;
+  }
+  for($j = 1; $j < strlen($i); $j++){
+    $newstr = substr($i, $j, strlen($i) - $j) . substr($i, 0, $j);
+    if(!isPrime($newstr)){
+      continue 2;
+    }
+  }
+  $c[] = $i;
+}
+
+print(print_r($c,true)."\n");
