c5641afcaa287a81b1f75e6416a8189c86d8e8bf
Cameron Brown <cameronthebrown@gmail.com>
2019-05-02 13:22:27 +0930

    problem 38 solved

---
+++ 38/38.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Take the number 192 and multiply it by each of 1, 2, and 3:
+
+192 × 1 = 192
+192 × 2 = 384
+192 × 3 = 576
+By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)
+
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
+*/
+
+for($i = 1; $i < 1E5/2; $i++){
+  $product = (string)$i;
+  for($j = 2; strlen($product) < 9; $j++){
+    $product .= (string)($i * $j);
+  }
+  $j -= 1;
+  if(strlen($product) == 9 && $j > 1){
+    $test = str_split($product);
+    sort($test);
+    if($test == array(1, 2, 3, 4, 5, 6, 7, 8, 9)){
+      echo "{$product} - from {$i} and {$j}\n";
+    }
+  }
+}
